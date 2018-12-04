03edf17622933c29c54e8589aeaf80b1e7fac343
Stefan Nikolov <herr.nikolov@gmail.com>
2018-12-04 11:48:17 +0200

    Data Structures Course Initial

---
+++ 03.Others/Data_Structures_-_Dec_2018/01. Data-Structures-Linear-Data-Structures-and-Algorithm-Complexity/Lab/Lists/ArrayList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    private const int Capacity = 2;
+    private T[] data;
+
+    public ArrayList()
+    {
+        this.data = new T[Capacity];
+    }
+
+    public int Count
+    {
+        get;
+        private set;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            //throw new NotImplementedException();
+            return this.data[index];
+        }
+
+        set
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            this.data[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (this.Count >= this.data.Length)
+        {
+            this.Resize();
+        }
+        this.data[this.Count++] = item;
+    }
+
+    private void Resize()
+    {
+        T[] newArray = new T[this.Count * 2];
+        Array.Copy(this.data, newArray, this.Count);
+        this.data = newArray;
+
+    }
+
+    public T RemoveAt(int index)
+    {
+        if (index < 0 || index >= this.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        T item = this.data[index];
+
+        for (int i = index; i < this.Count; i++)
+        {
+            this.data[i] = this.data[i + 1];
+        }
+
+        this.Count--;
+
+        if (this.Count <= this.data.Length / 4)
+        {
+            this.Shrink();
+        }
+
+        return item;
+    }
+
+    private void Shrink()
+    {
+        T[] newArray = new T[this.data.Length / 2];
+        Array.Copy(this.data, newArray, this.Count);
+        this.data = newArray;
+    }
+}
---
+++ 03.Others/Data_Structures_-_Dec_2018/01. Data-Structures-Linear-Data-Structures-and-Algorithm-Complexity/Lab/Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList<int> list = new ArrayList<int>();
+        list.Add(10);
+        list.Add(10);
+        list.Add(10);
+        list.RemoveAt(1);
+        System.Console.WriteLine();
+    }
+}
