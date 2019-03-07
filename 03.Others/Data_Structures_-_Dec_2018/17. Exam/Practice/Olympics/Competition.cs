5935b767cba56100240c87052897a9c2d2553916
Stefan Nikolov <herr.nikolov@gmail.com>
2019-03-07 14:45:28 +0200

    Prep

---
+++ 03.Others/Data_Structures_-_Dec_2018/17. Exam/Practice/Olympics/Competition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public class Competition
+{
+    public Competition(string name, int id, int score)
+    {
+        this.Name = name;
+        this.Id = id;
+        this.Score = score;
+
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Score { get; set; }
+
+    public ICollection<Competitor> Competitors { get; set; }
+
+    public void AddCompetitor(Competitor competitor)
+    {
+        competitor.TotalScore = competitor.TotalScore + this.Score;
+        this.Competitors.Add(competitor);
+    }
+
+    public bool Disqualify(Competitor competitor)
+    {
+        bool isSuccess = this.Competitors.Remove(competitor);
+        if (isSuccess)
+        {
+            competitor.TotalScore = competitor.TotalScore - this.Score;
+        }
+        return isSuccess;
+    }
+}
---
+++ 03.Others/Data_Structures_-_Dec_2018/17. Exam/Practice/Olympics/Competitor.cs
@@ -0,0 +1,25 @@
+﻿public class Competitor
+{
+    public Competitor(int id, string name)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.TotalScore = 0;
+    }
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public long TotalScore { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        return this.Id == ((Competitor)obj).Id;
+    }
+    public override int GetHashCode()
+    {
+        return this.Id;
+    }
+
+}
